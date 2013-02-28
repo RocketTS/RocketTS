5c98817e6a45dc2cfba115dfc0ebca46e6a77342
Matthias <code@hias.me>
2013-02-28 12:35:23 +0100

    xampp.bat updated

--- xampp.bat
+++ xampp.bat
@@ -4,8 +4,8 @@
 copy cgi\* C:\xampp\cgi-bin\RocketTS\
 
 #CSS Dateien auf XAMPP kopieren
-copy css\* C:\xampp\htdocs\css\
+copy css\* C:\xampp\htdocs\rocket\css\
 
 #Restliche Dateien auf XAMPP kopieren
-copy sql\* C:\xampp\htdocs\sql\
-copy doku\* C:\xampp\htdocs\doku\
+copy sql\* C:\xampp\htdocs\rocket\sql\
+copy doku\* C:\xampp\htdocs\rocket\doku\
